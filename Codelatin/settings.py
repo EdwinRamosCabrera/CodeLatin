07fb582b500e0e4d5e0177fefd41b5ec37104630
Edwin Ramos <edwin_ramos2@usmp.pe>
2025-10-17 18:51:45 -0500

    Creacion de una vista simple con funciones y adicion de cloudflare

--- Codelatin/settings.py
+++ Codelatin/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-q8w1^5fmi4&pzgv(3_+*zy3rroy^i+bct7a$6+er%+5p#^=p&0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.trycloudflare.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'Codelatin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
